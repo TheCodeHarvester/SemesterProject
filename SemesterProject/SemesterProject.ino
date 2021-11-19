81044f30ceb9beed1e0ea601067f76fd55fb2b56
mdesear <82544367+mdesear@users.noreply.github.com>
2021-11-18 23:25:08 -0800

    Sensor code
    Entered code for temp/humidity sensor; need to include DHT library

--- SemesterProject/SemesterProject.ino
+++ SemesterProject/SemesterProject.ino
@@ -1,4 +1,17 @@
 //Settings
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHTPIN 2
+
+#define DHTTYPE DH22
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
+
 float tempSet = 0;
 int angleSet = 0;
 
@@ -8,7 +21,29 @@ int lastState = 0, state = 0, waterLevel = 0, vAngle;
 
 
 void setup() {
-  temp = temp(0)
+	Serial.begin(9600);
+	dht.begin();
+	sensor_t sensor;
+	dht.temperature().getSensor(&sensor);
+	Serial.println(F("------------------------------------");
+	Serial.println(F("Temperature Sensor"));
+	Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+	Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+	Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+	Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+	Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+	Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+	Serial.println(F("------------------------------------"));
+	dht.humidity().getSensor(&sensor);
+	Serial.println(F("Humidity Sensor"));
+	Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+	Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+	Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+	Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+	Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+	Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+	Serial.println(F("------------------------------------"));
+	delayMS = sensor.min_delay / 1000;
 }
 
 void loop() {
@@ -19,6 +54,26 @@ void loop() {
   //Check for state change and save info if changed.
   if(lastState != state){
     Save();
+	
+  delay(delayMS);
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
   }
     
   //Idle State
